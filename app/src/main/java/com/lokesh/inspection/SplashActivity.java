cdcc045b02b61aa71e87efa6d2461e2d11f03560
Lokesh Vishnoi <lokeshvishnoi1@gmail.com>
2023-08-06 15:33:46 +0530

    Added splash screen

---
+++ app/src/main/java/com/lokesh/inspection/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.lokesh.inspection;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends Activity {
+
+    private final static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();  //close this activity
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
