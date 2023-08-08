17279c5ccc578e60733578e59d653c42fcb49499
Lokesh Vishnoi <lokeshvishnoi1@gmail.com>
2023-08-08 19:40:48 +0530

    Fixed purple color issue

--- app/src/main/java/com/lokesh/inspection/SplashActivity.java
+++ app/src/main/java/com/lokesh/inspection/SplashActivity.java
@@ -1,13 +1,12 @@
 package com.lokesh.inspection;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-@SuppressLint("CustomSplashScreen")
-public class SplashActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
 
     private final static int SPLASH_TIME_OUT = 3000;
 
@@ -16,14 +15,10 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-                Intent i = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(i);
-                finish();  //close this activity
-            }
+        new Handler().postDelayed(() -> {
+            Intent i = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(i);
+            finish();
         }, SPLASH_TIME_OUT);
     }
 }
